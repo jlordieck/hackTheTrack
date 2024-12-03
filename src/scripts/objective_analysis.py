7f6b93a0c1795d45a96130486ecebc38613d58ea
Thomas Dubach <thomas.dubach@ivt.baug.ethz.ch>
2024-12-03 16:59:31 +0100

    update objective analysis

--- src/scripts/objective_analysis.py
+++ src/scripts/objective_analysis.py
@@ -9,42 +9,59 @@ from hackthetrack.statistics_logger import StatisticsLogger
 logger = StatisticsLogger()
 displib_directory = Path("out/instances/displib_instances_phase1")
 
-for path_to_instance in displib_directory.glob("*.json"):
+for path_to_instance in filter(lambda x: True, displib_directory.glob("*.json")):
     instance = DisplibInstance.from_json(path_to_instance)
     network = DependencyGraph.from_displib_instance(instance)
 
     objectives = instance.objectives
-    thresholds = []
-    lower_bounds = []
-    upper_bounds = []
-    train_ids = []
+    thresholds, lower_bounds, upper_bounds, train_ids, objective_types = [], [], [], [], []
+    objective_on_last_node = dict()
+
+    all_train_ids = [train["id"] for train in instance.trains]
+    for train_id in all_train_ids:
+        objective_on_last_node[train_id] = False
 
     for objective in objectives:
         train_id = objective["train"]
         train_ids.append(train_id)
         operation = objective["operation"]
         node = network.node_by_train_id_and_index(train_id, operation)
+        last_index = instance.trains[train_id]["operations"][-1].index
+
+        if node.index == last_index:
+            objective_on_last_node[train_id] = True
+
         thresholds.append(objective["threshold"])
-        if node.start_lb is None:
-            lower_bounds.append(0.0)
-        else:
-            lower_bounds.append(node.start_lb)
+        lower_bounds.append(node.start_lb if node.start_lb is not None else 0.0)
         upper_bounds.append(node.start_ub)
 
-    if len(train_ids) == len(set(train_ids)):
-        logger.update_instance(path_to_instance.stem, "one objective per train", True)
-    else:
-        logger.update_instance(path_to_instance.stem, "one objective per train", False)
+        assert objective["coeff"] or objective["increment"]
+        if objective["coeff"] and objective["increment"]:
+            objective_types.append("mixed")
+        else:
+            objective_types.append("coeff" if objective["coeff"] != 0.0 else "increment")
 
-    if all([lb == threshold for lb, threshold in zip(lower_bounds, thresholds)]):
-        logger.update_instance(path_to_instance.stem, "lower bounds == thresholds", True)
-    else:
-        logger.update_instance(path_to_instance.stem, "lower bounds == thresholds", False)
+    all_trains_have_objective = len(set(train_ids)) == len(all_train_ids)
+    logger.update_instance(path_to_instance.stem, "all trains have an objective", all_trains_have_objective)
 
-    if all([ub == None for ub in upper_bounds]):
-        logger.update_instance(path_to_instance.stem, "no upper bounds on any objective node", True)
-    else:
-        logger.update_instance(path_to_instance.stem, "no upper bounds on any objective node", False)
+    one_objective_per_train = len(train_ids) == len(set(train_ids))
+    logger.update_instance(path_to_instance.stem, "one objective per train", one_objective_per_train)
+
+    objectives_on_last_node = all(objective_on_last_node.values())
+    logger.update_instance(path_to_instance.stem, "objective always on last node", objectives_on_last_node)
+
+    lower_bounds_equal_thresholds = all([lb == threshold for lb, threshold in zip(lower_bounds, thresholds)])
+    logger.update_instance(path_to_instance.stem, "lower bounds == thresholds", lower_bounds_equal_thresholds)
+
+    no_upper_bounds = all([ub == None for ub in upper_bounds])
+    logger.update_instance(path_to_instance.stem, "no upper bounds on any objective node", no_upper_bounds)
+
+    objective_type = "coeff" if all([objective_type == "coeff" for objective_type in objective_types]) else "mixed"
+    if objective_type == "mixed":
+        objective_type = (
+            "increment" if all([objective_type == "increment" for objective_type in objective_types]) else "mixed"
+        )
+    logger.update_instance(path_to_instance.stem, "objective type", objective_type)
 
     plot = False
     if plot:
@@ -54,7 +71,7 @@ for path_to_instance in displib_directory.glob("*.json"):
         fig.add_trace(
             go.Scatter(
                 x=train_ids,
-                y=[upper - lower for upper, lower in zip(upper_bounds, lower_bounds)],
+                y=[upper - lower if upper is not None else None for upper, lower in zip(upper_bounds, lower_bounds)],
                 mode="markers",
                 marker=dict(size=10, color="blue"),
                 name="Upper bound relative to lower bound",
