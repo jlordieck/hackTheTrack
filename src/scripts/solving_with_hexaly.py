e8fd8ea83a442d6ba8ea06a8229833c5eb237719
Thomas Dubach <thomas.dubach@ivt.baug.ethz.ch>
2024-12-11 16:46:07 +0100

    [HEXALY] update

--- src/scripts/solving_with_hexaly.py
+++ src/scripts/solving_with_hexaly.py
@@ -6,7 +6,10 @@ from hackthetrack.displib.load_displib_instance import DisplibInstance
 
 displib_instance_path = Path("out/instances/displib_instances_phase1/line1_critical_0.json")
 instance = DisplibInstance.from_json(displib_instance_path)
+
 network = DependencyGraph.from_displib_instance(instance)
+objective_nodes = [network.node_by_train_id_and_index(obj["train"], obj["operation"]) for obj in instance.objectives]
+objective_node_indices = [node.index for node in objective_nodes]
 
 operations = [node.index for node in network.all_nodes]
 num_operations = len(network.all_nodes)
@@ -14,16 +17,18 @@ num_operations = len(network.all_nodes)
 with hexaly.optimizer.HexalyOptimizer() as optimizer:
     model = optimizer.get_model()
 
-    operations = [model.interval(0, horizon) for _ in range(num_operations)]
+    operations = [model.interval(node.start_lb if node.start_lb else 0, horizon) for node in network.all_nodes]
     operation_array = model.array(operations)
 
     resource_allocation = model.array([model.list(num_operations) for _ in range(num_resources)])
 
     operation_to_train_array = model.array(operation_to_train)
 
-    # lower bound on operation duration
-    for operation, duration in zip(operations, durations):
-        model.add_constraint(model.length(operation) >= duration)
+    # lower bound on operation duration and upper bound on start time if applicable
+    for i, operation in enumerate(operations):
+        model.add_constraint(model.length(operation) >= network.all_nodes[i].min_duration)
+        if network.all_nodes[i].start_ub is not None:
+            model.add_constraint(model.start(operation) <= network.all_nodes[i].start_ub)
 
     # ordering of operations on the same resource with release times if subsequent operations are on different trains
     for r in range(num_resources):
@@ -55,3 +60,9 @@ with hexaly.optimizer.HexalyOptimizer() as optimizer:
                     ]
                 )
             )
+
+    # objective
+    model.minimize(model.sum(model.end(operation_array[i]) for i in objective_node_indices))
+    model.close()
+
+    optimizer.solve()
